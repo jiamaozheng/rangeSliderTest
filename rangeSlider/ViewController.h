a1ef94d0e2c4cade3e3985c3c1adbd348e49ba18
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-28 15:49:02 -0500

    addda

--- rangeSlider/ViewController.h
+++ rangeSlider/ViewController.h
@@ -8,12 +8,14 @@
 
 #import <UIKit/UIKit.h>
 #import "NMRangeSlider.h"
+#import "CERangeSlider.h"
 
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet NMRangeSlider *labelSlider;
 @property (weak, nonatomic) IBOutlet UILabel *lowerLabel;
 @property (weak, nonatomic) IBOutlet UILabel *upperLabel;
+@property (weak, nonatomic) IBOutlet CERangeSlider *_rangeSlider;
 
 - (IBAction)labelSliderChanged:(NMRangeSlider*)sender;
 @end
--- rangeSlider/ViewController.m
+++ rangeSlider/ViewController.m
@@ -17,7 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self configureLabelSlider];
+    
+    
+    NSUInteger margin = 50;
+    CGRect sliderFrame = CGRectMake(margin, margin, self.view.frame.size.width - margin * 2, 30);
+    self._rangeSlider = [[CERangeSlider alloc] initWithFrame:sliderFrame];
+    
+    [self.view addSubview:self._rangeSlider];
+    
+    [self._rangeSlider addTarget:self
+                     action:@selector(slideValueChanged:)
+           forControlEvents:UIControlEventValueChanged];
+    
+    [self performSelector:@selector(updateState) withObject:nil afterDelay:1.0f];
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)slideValueChanged:(id)control
+{
+    NSLog(@"Slider value changed: (%.2f,%.2f)",
+          self._rangeSlider.lowerValue,self._rangeSlider.upperValue);
+}
+
+- (void)updateState
+{
+    self._rangeSlider.trackHighlightColour = [UIColor redColor];
+    self._rangeSlider.curvatiousness = 0.0;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,12 +55,12 @@
 {
     [super viewWillAppear:animated];
     
-//    [self updateSliderLabels];
-//
-//    if([self.view respondsToSelector:@selector(setTintColor:)])
-//    {
-//        self.view.tintColor = [UIColor orangeColor];
-//    }
+    [self updateSliderLabels];
+
+    if([self.view respondsToSelector:@selector(setTintColor:)])
+    {
+        self.view.tintColor = [UIColor orangeColor];
+    }
     
 }
 
@@ -67,6 +93,8 @@
     upperCenter.y = (self.labelSlider.center.y - 30.0f);
     self.upperLabel.center = upperCenter;
     self.upperLabel.text = [NSString stringWithFormat:@"%d", (int)self.labelSlider.upperValue];
+//    self.upperLabel.hidden = YES;
+
 }
 
 // Handle control value changed events just like a normal slider
