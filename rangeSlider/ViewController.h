5eb3636c086be9b8308209f37f46825e5dc3a6fc
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-28 14:01:07 -0500

    dsfsdfsdf

--- rangeSlider/ViewController.h
+++ rangeSlider/ViewController.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "NMRangeSlider.h"
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet NMRangeSlider *labelSlider;
+@property (weak, nonatomic) IBOutlet UILabel *lowerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *upperLabel;
 
+- (IBAction)labelSliderChanged:(NMRangeSlider*)sender;
 @end
 
--- rangeSlider/ViewController.m
+++ rangeSlider/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self configureLabelSlider];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +24,56 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+//    [self updateSliderLabels];
+//
+//    if([self.view respondsToSelector:@selector(setTintColor:)])
+//    {
+//        self.view.tintColor = [UIColor orangeColor];
+//    }
+    
+}
+
+#pragma mark -
+#pragma mark - Label  Slider
+
+- (void) configureLabelSlider
+{
+    self.labelSlider.minimumValue = 0;
+    self.labelSlider.maximumValue = 100;
+    
+    self.labelSlider.lowerValue = 0;
+    self.labelSlider.upperValue = 100;
+    
+    self.labelSlider.minimumRange = 10;
+}
+
+- (void) updateSliderLabels
+{
+    // You get get the center point of the slider handles and use this to arrange other subviews
+    
+    CGPoint lowerCenter;
+    lowerCenter.x = (self.labelSlider.lowerCenter.x + self.labelSlider.frame.origin.x);
+    lowerCenter.y = (self.labelSlider.center.y - 30.0f);
+    self.lowerLabel.center = lowerCenter;
+    self.lowerLabel.text = [NSString stringWithFormat:@"%d", (int)self.labelSlider.lowerValue];
+    
+    CGPoint upperCenter;
+    upperCenter.x = (self.labelSlider.upperCenter.x + self.labelSlider.frame.origin.x);
+    upperCenter.y = (self.labelSlider.center.y - 30.0f);
+    self.upperLabel.center = upperCenter;
+    self.upperLabel.text = [NSString stringWithFormat:@"%d", (int)self.labelSlider.upperValue];
+}
+
+// Handle control value changed events just like a normal slider
+- (IBAction)labelSliderChanged:(NMRangeSlider*)sender
+{
+    [self updateSliderLabels];
+}
+
 
 @end
